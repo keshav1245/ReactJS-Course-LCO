ac32d0e03b1b23b119e6174711b248f350f78e03
keshav1245 <tangri57@gmail.com>
2021-11-21 17:52:48 +0530

    React JS course, helloworld program, hello world from scrate. Reusability

---
+++ 02-helloworld-scratch/src/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Button = ({title="Just for fun"}) => (
+        <button className="button">{title}</button>
+)
+
+
+export default Button
---
+++ 02-helloworld-scratch/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app'
+
+
+
+
+//Render takes 2 args, => What I want to render, Where I want to render
+//The app is an element 
+ReactDOM.render(<App/>, document.getElementById('root')); 
